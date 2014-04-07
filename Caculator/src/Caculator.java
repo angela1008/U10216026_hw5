20af34dd976d29fc6c23f5364e9c2003c382596f
angela1008 <angel.app2682@gmail.com>
2014-04-07 22:08:12 +0800

    Seventh programming
    最後完成註解

--- Caculator/src/Caculator.java
+++ Caculator/src/Caculator.java
@@ -4,7 +4,10 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
+// Create a class extends JFrame
 public class Caculator extends JFrame {
+	
+	// Create buttons for calculator
 	private JButton on = new JButton("ON");
 	private JButton delete = new JButton("←");
 	private JButton clear = new JButton("CE");
@@ -19,68 +22,84 @@ public class Caculator extends JFrame {
 	private JButton n0 = new JButton("" + 0);
 	private JButton decimalpoint = new JButton(".");
 	private JButton equal = new JButton("=");
-
+	
+	// Create an array to set number 1~9
 	private JButton digits[] = new JButton[10];
 	
+	// Create a label to show the number and set initial value 0 right
 	private static Label lab = new Label("0", Label.RIGHT);
 	
 	private long result;// 存放結果
 	private byte op;// 代表運算子
-
+	
+	// Constructor of Caculator
 	public Caculator() {
-
+		
+		// Create panel p1 (set GridLayout, 4 rows, 3 columns) for some buttons and number 1~9
 		JPanel p1 = new JPanel(new GridLayout(4, 3));
+		
+		// Add buttons
 		p1.add(on);
 		p1.add(delete);
 		p1.add(clear);
+		
+		// Add number 1~9
 		for (int i = 1; i <= 9; i++) {
-			digits[i] = new JButton(Integer.toString(i));
+			digits[i] = new JButton(Integer.toString(i)); // Convert i from Int type to String type
 			p1.add(digits[i]);
-			digits[i].addActionListener(new ButtonListener());
+			digits[i].addActionListener(new ButtonListener()); // Register listener
 		}
-
+		
+		// Create panel p2 (set GridLayout, 4 rows, 2 columns) for some buttons
 		JPanel p2 = new JPanel(new GridLayout(4, 2));
 		p2.add(clearall);
-		clearall.addActionListener(new ButtonListener());
+		clearall.addActionListener(new ButtonListener()); // Register listener
 		p2.add(plus);
-		plus.addActionListener(new ButtonListener());
+		plus.addActionListener(new ButtonListener()); // Register listener
 		p2.add(pandn);
 		p2.add(minus);
-		minus.addActionListener(new ButtonListener());
+		minus.addActionListener(new ButtonListener()); // Register listener
 		p2.add(root);
 		p2.add(multiply);
-		multiply.addActionListener(new ButtonListener());
+		multiply.addActionListener(new ButtonListener()); // Register listener
 		p2.add(countdown);
 		p2.add(divide);
-		divide.addActionListener(new ButtonListener());
-
+		divide.addActionListener(new ButtonListener()); // Register listener
+		
+		// Create panel p3 (set GridLayout, 1 rows, 5 columns) for some buttons and number 0
 		JPanel p3 = new JPanel(new GridLayout(1, 5));
 		p3.add(n0);
-		n0.addActionListener(new ButtonListener());
+		n0.addActionListener(new ButtonListener()); // Register listener
 		p3.add(decimalpoint);
 		p3.add(equal);
-		equal.addActionListener(new ButtonListener());
+		equal.addActionListener(new ButtonListener()); // Register listener
 
+		// Create panel p4
 		JPanel p4 = new JPanel(new BorderLayout());
+		
+		// Add label lab for north, panel p1 for center, panel p2 for east, panel p3 for south
 		p4.add(lab, BorderLayout.NORTH);
 		p4.add(p1, BorderLayout.CENTER);
 		p4.add(p2, BorderLayout.EAST);
 		p4.add(p3, BorderLayout.SOUTH);
 
+		// Add panel p4 to the frame
 		add(p4);
 		
-		this.setTitle("Caculator");
-		this.setSize(300, 350);
-		this.setResizable(false);
-		this.setLocationRelativeTo(null);
+		// Set the frame
+		this.setTitle("Caculator"); // Name of the frame
+		this.setSize(300, 350); // Size of the frame
+		this.setResizable(false); // User cannot change the size of frame
+		this.setLocationRelativeTo(null); // Center the frame
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setVisible(true);
+		this.setVisible(true); // Show the frame
 	}
 
+	// Handle the buttons
 	private class ButtonListener implements ActionListener{
 		public void actionPerformed(ActionEvent e){
 			
-			long num;// 存放由字串轉成的數值
+			long num; // 存放由字串轉成的數值
 			JButton btn = (JButton) e.getSource();
 
 			// 處理數值1-9
@@ -90,34 +109,34 @@ public class Caculator extends JFrame {
 			}
 
 			if (btn == clearall) {
-				num = 0;// 把儲存的結果歸0
+				num = 0; // 把儲存的結果歸0
 				result = 0;
 				op = 0;
 				lab.setText(Long.toString(result));
 			} 
 			
-			else if (btn == plus) {// 加
+			else if (btn == plus) { // 加
 				save_num(plus);
 				op = 1;
 			} 
 			
-			else if (btn == minus) {// 減
+			else if (btn == minus) { // 減
 				save_num(minus);
 				op = 2;
 			} 
 			
-			else if (btn == multiply) {// 乘
+			else if (btn == multiply) { // 乘
 				save_num(multiply);
 				op = 3;
 			} 
 			
-			else if (btn == divide) {// 除
+			else if (btn == divide) { // 除
 				save_num(divide);
 				op = 4;
 			} 
 			
 			else if (btn == equal) {
-				num = Long.parseLong(lab.getText());// 存放由字串轉成的數值
+				num = Long.parseLong(lab.getText()); // 存放由字串轉成的數值
 				
 				switch (op) {				
 				case 1:
@@ -136,7 +155,7 @@ public class Caculator extends JFrame {
 					try {
 						result /= num;						
 					} 
-					catch (ArithmeticException ae) {
+					catch (ArithmeticException ae) { // 以防被除數為0
 						result = 0;
 						num = 0;
 						System.out.println("被除數不得為0");
@@ -146,7 +165,7 @@ public class Caculator extends JFrame {
 				default:
 				}
 
-				num = 0;
+				num = 0; // 把儲存的結果歸0
 
 				// 輸出運算後的結果到顯示器
 				lab.setText(Long.toString(result));
@@ -164,9 +183,4 @@ public class Caculator extends JFrame {
 			lab.setText(Long.toString(0));
 		}		
 	}
-	
-//	public static void main(String[] args) {
-//		Caculator frame = new Caculator();
-//		
-//	}
 }
--- Caculator/src/password.java
+++ Caculator/src/password.java
@@ -3,46 +3,64 @@ import java.awt.event.*;
 
 import javax.swing.*;
 
+// Create a class extends JFrame
 public class password extends JFrame {
 	
-	public String sp;
-	private JLabel password = new JLabel("Password");
-	private JTextField Jtfpassword = new JTextField("Deafult:(0000).", 10);
-	private JButton ok = new JButton("OK");
+	public String sp; // Store the text
+	private JLabel password = new JLabel("Password"); // Create a label to show the "password"
+	private JTextField Jtfpassword = new JTextField("Deafult:(0000).", 10); // Create a text field for user to enter the password and prompt the default value is (0000)
+	private JButton ok = new JButton("OK"); // Create a button to confirm the password
 	
+	// Constructor of password
 	public password(){
+		
+		// Create panel p1
 		JPanel p1 = new JPanel();
+		
+		// Add label, text field, and button
 		p1.add(password);
 		p1.add(Jtfpassword);
 		p1.add(ok);
 		
+		// Add panel p1 to the center of the frame
 		add(p1, BorderLayout.CENTER);
 		
+		// Register listener
 		ok.addActionListener((ActionListener) new ButtonListener());
 		
-		this.setTitle("Enter password");
-		this.setSize(400, 70);
-		this.setVisible(true);
+		// Set the frame
+		this.setTitle("Enter password"); // Name of the frame
+		this.setSize(400, 70); // Size of the frame
+		this.setVisible(true); // Show the frame
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setLocationRelativeTo(null);
-		this.setResizable(false);		
+		this.setLocationRelativeTo(null); // Center the frame
+		this.setResizable(false); // User cannot change the size of frame
 
 	}
 	
+	// Handle the "ok" buttons
 	class ButtonListener implements ActionListener{
 		public void actionPerformed(ActionEvent e){
-
+			
+			// Get the text
 			sp = Jtfpassword.getText();
-			if (sp.equals("0000")) {
-				new Caculator(); // If password is right, then invoke Calculator()
-			} else {
+			
+			// If password is right, then invoke Calculator()
+			if (sp.equals("0000"))
+				new Caculator(); 
+			
+			// Else clear the text and printout the "Error"
+			else {
 				Jtfpassword.setText("");
 				System.out.println("Error");
 			}
 		}
 	}
 	
+	/** Main method */
 	public static void main(String[] args) {
+		
+		// Invoke the windows of entering password
 		password p = new password();
 	}
 }
